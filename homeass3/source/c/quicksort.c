cf5f39a392f758fa22758c33baae68d253ef2751
Overlord <TheExplosiveSheep@gmail.com>
2013-11-30 16:06:31 +0100

    add record support to quicksort and bubblesort

--- homeass3/source/c/quicksort.c
+++ homeass3/source/c/quicksort.c
@@ -1,24 +1,32 @@
 #include <stdio.h>
 
 #include "records.h"
-void quicksort( int a[], int l, int r ) {
-	int v, i, j, t;
+
+#define RECORD_LENGTH 7
+#define ARRAY_LENGTH 1000
+
+void quicksort( int a[][ RECORD_LENGTH ], int l, int r ) {
+	int v, i, j, t[ RECORD_LENGTH ], ti;
 	if ( r > l ) {
-		v = a[ r ];
+		v = a[ r ][ 0 ];
 		i = l - 1;
 		j = r;
 
 		for ( ;; ) {
-			while ( a[ ++i ] < v );
-			while ( a[ --j ] > v );
+			while ( a[ ++i ][ 0 ] < v );
+			while ( a[ --j ][ 0 ] > v );
 			if ( i >= j ) break;
-			t = a[ i ];
-			a[ i ] = a[ j ];
-			a[ j ] = t;
+			for ( ti = 0; ti < RECORD_LENGTH; ti++ ) {
+				t[ ti ] = a[ i ][ ti ];
+				a[ i ][ ti ] = a[ j ][ ti ];
+				a[ j ][ ti ] = t[ ti ];
+			}
 		}
-		t = a[ i ];
-		a[ i ] = a[ r ];
-		a[ r ] = t;
+		for ( ti = 0; ti < RECORD_LENGTH; ti++ ) {
+			t[ ti ] = a[ i ][ ti ];
+			a[ i ][ ti ] = a[ r ][ ti ];
+			a[ r ][ ti ] = t[ ti ];
+		}
 
 		quicksort( a, l, i - 1 );
 		quicksort( a, i + 1, r );
@@ -26,5 +34,7 @@ void quicksort( int a[], int l, int r ) {
 }
 
 int main() {
+
+	quicksort( records, 0, ARRAY_LENGTH );
 
 }
--- homeass3/source/c/test1.c
+++ homeass3/source/c/test1.c
@@ -12,7 +12,7 @@ int main() {
 	for ( i = 0; i < TAB_SIZE; i++ ) {
 		A[ i ] = 1;
 	}
-
+	
 	//Data referencing
 	for ( i = 0; i < TAB_SIZE - CACHE_SIZE; i++ ) {
 		for ( j = 0; j < 5; j++ ) {
@@ -20,7 +20,7 @@ int main() {
 			sum += A[ i + CACHE_SIZE ];
 		}
 	}
-
+	
 	printf( "Sum = %d\n", sum );
 	return 0;
 }
